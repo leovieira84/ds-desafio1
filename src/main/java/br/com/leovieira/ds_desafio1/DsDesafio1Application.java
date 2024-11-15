34ead6c9cb4daae21d97397e76dfb50480e314b0
leovieira84 <leonardo.vieira@gmail.com>
2024-11-15 16:03:10 -0300

    Configuração inicial do projeto - Spring Initializer

---
+++ src/main/java/br/com/leovieira/ds_desafio1/DsDesafio1Application.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.ds_desafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsDesafio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leovieira/ds_desafio1/DsDesafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.ds_desafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDesafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
